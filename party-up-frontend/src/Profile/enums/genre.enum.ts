1f57e7f0f4dbf143ea356d15d7d14c9b719a640d
Yoan <yoan.dubuc@outlook.com>
2021-03-08 17:54:13 -0500

    add more genres

--- party-up-frontend/src/Profile/enums/genre.enum.ts
+++ party-up-frontend/src/Profile/enums/genre.enum.ts
@@ -2,6 +2,15 @@ enum Genre {
   ACTION = 'Action',
   ADVENTURE = 'Adventure',
   PLATFORMER = 'Platformer',
+  SHOOTER = 'Shooter',
+  HORROR = 'Horror',
+  FIGHTING = 'Fighting',
+  BATTLE_ROYALE = 'Battle Royale',
+  RHYTHM = 'Rhythm',
+  SPORTS = 'Sports',
+  RPG = 'RPG',
+  MMO = 'MMO',
+  STRATEGY = 'Strategy',
 }
 
 export default Genre;
