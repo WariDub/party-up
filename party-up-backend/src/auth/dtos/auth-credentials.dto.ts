ca10768955b863493861eab07c62a466a033d0b7
Yoan <yoan.dubuc@outlook.com>
2021-04-23 17:09:49 -0400

    improved auth error messages

--- party-up-backend/src/auth/dtos/auth-credentials.dto.ts
+++ party-up-backend/src/auth/dtos/auth-credentials.dto.ts
@@ -13,19 +13,22 @@ export class AuthCredentialsDto {
     @IsOptional()
     @IsString()
     @Length(4, 24)
-    @Matches(/[A-Za-z0-9]/)
+    @Matches(/[A-Za-z0-9]/, { message: 'username can only contain letters and numbers' })
     @IsLowercase()
     readonly username: string;
 
     @IsOptional()
     @IsString()
     @MinLength(5)
-    @IsEmail()
+    @IsEmail({}, { message: 'email must be valid' })
     readonly email: string;
 
     @IsString()
     @MinLength(8)
     @MaxLength(255)
-    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message:
+            'password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or special character',
+    })
     readonly password: string;
 }
