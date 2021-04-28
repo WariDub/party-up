f672404f06cd0af06f11c3ad8b2601fd13f82d93
Justin Ross <justinbross100@gmail.com>
2021-04-28 15:05:04 -0400

    End of day commit
    Commiting the unit test I have fixed

--- party-up-frontend/src/Auth/Test/AuthenticationForm.react.test.tsx
+++ party-up-frontend/src/Auth/Test/AuthenticationForm.react.test.tsx
@@ -1,11 +1,10 @@
 import { shallow, mount } from 'enzyme';
 import { AuthenticationForm }  from '../AuthenticationForm';
-import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import renderer from 'react-test-renderer';
-
-let container: any = null;
-
+import screen, { render, getByLabelText, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import sinon from 'sinon'
 describe('AuthenticationForm', () => {
 
     test('It should mount', () => {
@@ -13,24 +12,10 @@ describe('AuthenticationForm', () => {
 
         expect(component.length).toBe(1);
     });
-    
-    test('It displays the Authentication Form', () => {
-        let container: any = null;
-
-        container = document.createElement("div");
-        document.body.appendChild(container);
-
-        act(() => {
-            render(<AuthenticationForm />, container);
-        });
-
-        expect(container.innerText).toBe(undefined);
-    });
 
     test('It should contain all of original elements, and match the snapshot', () => {
         let component = renderer.create(<AuthenticationForm />).toJSON();
 
         expect(component).toMatchSnapshot();
     });
-    
 });
--- party-up-frontend/src/Search/Tests/NavBar.react.test.tsx
+++ party-up-frontend/src/Search/Tests/NavBar.react.test.tsx
@@ -5,6 +5,11 @@ import { Game } from '../../Search/interfaces/Game';
 import { createBrowserHistory } from 'history'
 import { matchPath } from 'react-router';
 import sinon from 'sinon';
+import { Button } from '@material-ui/core';
+import screen, { render, getByLabelText, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+
 
 describe('NavBar', () => {
     test('Should render without crashing', () => {
@@ -31,6 +36,18 @@ describe('NavBar', () => {
         let component = shallow(<NavBar showSearchBar = { true } onQueryResult = {null} history = { history } location = { location } match = { match } />)
 
         expect(component.length).toBe(1);
+    });
+
+    test('It should render a clickable profile button', () => {
+        const container = render(<NavBar/>);
+
+        expect(container.getByRole('button', {name: 'Profile'})).toBeDefined();
+    });
+
+    test('It should render a clickable Logout button', () => {
+        const container = render(<NavBar/>);
+
+        expect(container.getByRole('button', {name: 'Logout'})).toBeDefined();
     });
 
     test('The elements of the UI should all render, and be unchanged', () => {
@@ -81,5 +98,31 @@ describe('NavBar', () => {
         const spy = jest.spyOn( NavBar.prototype, 'renderSearchBar');
 
         expect(spy).toHaveBeenCalledTimes(0);
+    });
+
+    test('Should render search bar if showSearchBar is false', () => { 
+        let game: Game = {
+            id: 'TestId',
+            name: 'TestName'
+        }
+
+        let gameList: Game[] = [game];
+
+        let history = createBrowserHistory();
+        history.push('/');
+
+        let location = history.location;
+
+        let match = matchPath('/ProfilePage', {
+            path: '/ProfilePage',
+            exact: true,
+            strict: false,
+        });
+
+        let component = shallow(<NavBar showSearchBar = { true } onQueryResult = {null} history = { history } location = { location } match = { match } />)
+
+        const spy = jest.spyOn( NavBar.prototype, 'renderSearchBar');
+
+        expect(spy).toHaveBeenCalled();
     });
 });
